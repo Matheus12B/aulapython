2ef12ecfdad93ec06e1e409684b4b1f14d68e21c
Matheus12B <matheus.mbernardino@gmail.com>
2021-03-18 20:04:58 +0000

    Parte1

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,8 @@ from flask import render_template
 @app.route('/index')
 def index():
     user = {'username': 'Feulo'}
-    return render_template("index.html", user=user)
+    posts = [
+        {'author': {'username': 'Maria'}, 'body': "Olá da Maria"},
+        {'author': {'username': 'Mario'}, 'body': "Olá!"}
+    ]
+    return render_template("index.html", user=user, posts=posts)
