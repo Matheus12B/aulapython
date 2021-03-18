f738a448c365e894b256ea926061a4270168068b
Matheus12B <matheus.mbernardino@gmail.com>
2021-03-18 19:27:04 +0000

    Parte1

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World"
+    user = {'username': 'Feulo'}
+    return render_template("index.html", user=user)
