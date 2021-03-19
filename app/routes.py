f8235f7112ddf970a6e277006af8e334a11c8d61
Matheus12B <matheus.mbernardino@gmail.com>
2021-03-19 00:46:08 +0000

    parte final

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,21 @@
 from app import app, db
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
+from app.models import User, Post
 
 
 @app.route('/post', methods=["POST"])
 def post():
+    post = Post(author=current_user, body=request.values.get('post'))
+    db.session.add(post)
+    db.session.commit()
     return redirect(url_for('index'))
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'Feulo'}
-    posts = [
-        {'author': {'username': 'Maria'}, 'body': "Olá da Maria"},
-        {'author': {'username': 'Mario'}, 'body': "Olá!"}
-    ]
+    posts = Post.query.all()
     return render_template("index.html", user=current_user, posts=posts)
 
 @app.route('/login', methods=["GET", "POST"])
@@ -33,12 +32,10 @@ def login():
         return redirect(url_for('index'))
     return render_template("login.html" , title="Login")
 
-
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
-
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
